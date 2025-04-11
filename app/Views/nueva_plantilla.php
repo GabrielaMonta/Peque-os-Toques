40c8cf0a224919e9516c1562612ae3678dd1a2f1
bianca30ailin <ivbianca25@gmail.com>
2025-04-11 15:24:41 -0300

    configuracion nueva

--- app/Views/nueva_plantilla.php
+++ app/Views/nueva_plantilla.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Pequeños Toques </title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" >
     <link href="assets/css/estilo.css" rel="stylesheet" > 
   </head>
 
