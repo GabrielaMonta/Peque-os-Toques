8bfe1b7c57bc7e38473c06d08f898ba7153c19db
bianca30ailin <ivbianca25@gmail.com>
2025-04-23 08:39:13 -0300

    Contacto

--- app/Views/front/nosotros/contacto.php
+++ app/Views/front/nosotros/contacto.php
@@ -1,17 +1,46 @@
 <main>
-<h4 class="letra-sub-nosotros">CONTACTO</h4>
-<div class="container-quienessomos text-center">
-    <!-- Redes sociales centradas -->
-    <div class="text-center pt-1">
-                <a href="https://wa.me/5493718658018" class="text-dark me-3" target="_blank"><i class="bi bi-whatsapp fs-2"style="color: #9da53a;"></i></a>
-                <a href="https://www.facebook.com/profile.php?id=100062870595052" class="text-dark me-3" target="_blank"><i class="bi bi-facebook fs-2"style="color: #9da53a;"></i></a>
-                <a href="#" class="text-dark "><i class="bi bi-instagram fs-2"style="color: #9da53a;"></i></a>
+
+    <div class="container-contacto row d-flex flex-wrap">
+        
+        <div class="col-12 col-md-4 mt-5">
+                <h4 class="letra-sub-contacto">CONTACTO</h4>
+                
+                <div class="fila-contacto">
+                    <i class="icono-redes bi bi-whatsapp ms-5 fs-2" style= "color:rgb(0, 0, 0);"></i>
+                    <p class="parrafos-contacto">3718-658018</p>
+                </div>
+
+                <div class="fila-contacto">
+                    <i class="icono-redes bi bi-facebook ms-5 fs-2" style="color:rgb(0, 0, 0);"></i>
+                    <p class="parrafos-contacto"><a href="https://www.facebook.com/profile.php?id=100062870595052" target="_blank">Pequeños toques</a></p>
+                </div>
+
+                <div class="fila-contacto">
+                    <i class="icono-redes bi bi-instagram ms-5 fs-2" style="color:rgb(0, 0, 0);"></i>
+                    <p class="parrafos-contacto"><a href="https://www.instagram.com" target="_blank">Pequeños toques</a></p>
+                </div>
+        </div>  
+
+        <div class="col-12 col-md-4 d-none d-md-flex justify-content-center align-items-center ms-4" style="min-height: 200px;">
+            <a href="#">
+                <img src="assets/img/Pequeños toques t.png" alt="Logo" height="40">
+            </a>
+        </div>
+
+        <div class="container-formulario-consulta col-12 col-md-3 mt-3 ms-5">
+            
+            <p class="letra-consulta mt-2">¡Dejanos tu consulta!</p>
+                <form action="/enviar-consulta" method="post">
+                    <input class="input-nombre form-control mb-0" type="text" name="nombre" placeholder="Tu nombre" required><br><br>
+                    <input class="input-emailcon form-control mb-0" type="email" name="email" placeholder="Tu email" required><br><br>
+                    <textarea class="input-mensaje form-control mb-0" name="mensaje" placeholder="Tu consulta" required></textarea><br><br>
+                    
+                    <div style="text-align: right;">
+                        <button class="boton-enviarcon" type="submit">Enviar</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
     </div>
-
-    <h6 class= "preguntas-frecuentes mx-auto text-center mt-5"> Nos encontramos en...    </h6>
-                <p class="parrafos-nosotros ms-5 me-5">Hipólito Yrigoyen entre San Vicente y Los Andes, Clorinda, Formosa, Argentina.
-                <br>Lunes a viernes de 17 a 21hs / Sabados, domingos y feriados 9 a 21hs.</p>
-
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3607.2549674704856!2d-57.72314879038611!3d-25.29563637755445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x945d08c658fbbf25%3A0xf11444b8665322ab!2sHip%C3%B3lito%20Yrigoyen%201130%2C%20P3610CYX%20Clorinda%2C%20Formosa!5e0!3m2!1ses!2sar!4v1745106458933!5m2!1ses!2sar" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</div>
 </main>
